d287053ff6abc19cdeeb21395090126603beaf33
shivam <jhodshivam5122@gmail.com>
2023-09-13 11:13:48 +0530

    ..

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,7 +1,60 @@
-const Page = async() => {
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.action';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import { profileTabs } from '@/constants';
+import  Image  from 'next/image';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+import UserCard from '@/components/cards/UserCard';
+import { fetchCommunities } from '@/lib/actions/community.action';
+import CommunityCard from './../../../components/cards/CommunityCard';
+
+
+
+async function Page() {
+
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id)
+
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    // Fetch community
+
+    const result = await fetchCommunities({
+
+        searchString:'',
+        pageNumber: 1,
+        pageSize: 25,
+
+    })
     return(
         <section>
-            <h1 className="head-text mb-10">Community</h1>
+            <h1 className="head-text mb-10">Search</h1>
+
+            {/**Search bar */}
+
+            <div className='mt-14 flex flex-col gap-9'>
+                {result.communities.length === 0 ? (
+                    <p className='no-result'>No communities</p>
+                ) : (
+                    <>
+                        {result.communities.map((community) => (
+                            <CommunityCard
+                                key={community.id}
+                                id={community.id}
+                                name={community.name}
+                                username ={community.username}
+                                imgUrl={community.image}
+                                bio={community.bio}
+                                members={community.members}
+                            />
+                        ))}
+                    </>
+                )}
+            </div>
         </section>
     )
 }
--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import { usePathname, useRouter } from "next/navigation";
+import { useOrganization } from "@clerk/nextjs";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
 
 import {
   Form,
@@ -13,85 +14,70 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-
-// import { updateUser } from "@/lib/actions/user.action";
-import { ThreadValidation } from './../../lib/vaildations/thread';
-import { Button } from "../ui/button";
+import { ThreadValidation } from "@/lib/vaildations/thread";
 import { createThread } from "@/lib/actions/thread.action";
 
 
 
 interface Props {
-  user: {
-    id: string;
-    objectId: string;
-    username: string;
-    name: string;
-    bio: string;
-    image: string;
-  };
-  btnTitle: string;
+  userId: string;
 }
 
+function PostThread({ userId }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
 
+  const { organization } = useOrganization();
 
-function PostThread({userId}: {userId:string}){
+  const form = useForm<z.infer<typeof ThreadValidation>>({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
 
-    const router = useRouter();
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: organization ? organization.id : null,
+      path: pathname,
+    });
 
-    const pathname = usePathname();
+    router.push("/");
+  };
 
-    const form = useForm({
-        resolver: zodResolver(ThreadValidation),
-        defaultValues: {
-            thread: '',
-            accountId: userId,
-        }
-    })
-
-    const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
-        await createThread({
-            text: values.thread,
-            author: userId,
-            communityId: null,
-            path: pathname
-        });
-
-        router.push("/")
-    }
-
-    return (
+  return (
     <Form {...form}>
-    <form 
-      onSubmit={form.handleSubmit(onSubmit)} 
-      className="mt-10 flex flex-col justify-start gap-10">
+      <form
+        className='mt-10 flex flex-col justify-start gap-10'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
         <FormField
-        control={form.control}
-        name="thread"
-        render={({ field }) => (
-          <FormItem className="flex flex-col w-full gap-3">
-            <FormLabel className="text-base-semibold text-light-2">
-              Content
-            </FormLabel>
-            <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
-              <Textarea 
-                rows={15}
-                {...field}
-               />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+          control={form.control}
+          name='thread'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Content
+              </FormLabel>
+              <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                <Textarea rows={15} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <Button type="submit" className="bg-primary-500">
-            Post Thread
-      </Button>
-
+        <Button type='submit' className='bg-primary-500'>
+          Post Thread
+        </Button>
       </form>
     </Form>
-    )
+  );
 }
 
-export default PostThread
+export default PostThread;
